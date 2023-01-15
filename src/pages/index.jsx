7cf0a29b6ee6a2dff47060f7af7f3bb133bfd357
Wong Lok <wonglok@users.noreply.github.com>
2023-01-15 17:57:15 +0800

    fa

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,27 @@
+import useSWR from 'swr'
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb'
+const fetcher = (url) => fetch(url).then((res) => res.json())
+
+function YoApp() {
+  const { data, error } = useSWR('/api/item?id=falala', fetcher)
+
+  if (error) return 'An error has occurred.'
+  if (!data) return 'Loading...'
+  return <code>{JSON.stringify(unmarshall(data), null, 2)}</code>
+}
+
 export default function Inedx() {
   return (
     <>
       {/*  */}
 
+      <YoApp></YoApp>
+
       {/*  */}
     </>
   )
 }
+
 export async function getStaticProps() {
   return { props: { title: 'Landing' } }
 }
